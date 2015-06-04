bc793ab76aae11e3c90b581e946272072912197f
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-06-04 21:15:40 +1000

    adding isomorphic product search app using alt and iso

--- product-search/server.js
+++ product-search/server.js
@@ -2,19 +2,21 @@ import express from 'express'
 import React from 'react'
 import AltIsomorphicElement from './src/components/AltIsomorphicElement'
 import Iso from 'iso'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import favicon from 'serve-favicon'
+import path from 'path'
 
 let port = 8080
 let ip = '127.0.0.1'
-let bodyParser = require('body-parser')
-let cookieParser = require('cookie-parser')
-let favicon = require('serve-favicon')
-let path = require('path')
 let app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade')
 
@@ -29,7 +31,7 @@ function getProductsFromServer(cb) {
       {name: 'iPhone 5', category: 'Electronics', price: '$399.99', stocked: false, visible: true},
       {name: 'Nexus 7', category: 'Electronics', price: '$199.99', stocked: true, visible: true}
     ];
-
+    
     cb(mockData)
 
   }, 500)
@@ -48,14 +50,12 @@ app.get('/', function (req, res) {
     }
 
     let node = React.createElement(AltIsomorphicElement, { altStores: data })
-
-    res.render('layout', {
-      html: Iso.render(React.renderToString(node), { altStores: data }, { react: true })
-    })
-
+    let html = Iso.render(React.renderToString(node), { altStores: data }, { react: true })
+    res.render('layout', { html: html })
   });
 
 })
+
 
 /* logging to the server */
 app.listen(port,ip, function() {
